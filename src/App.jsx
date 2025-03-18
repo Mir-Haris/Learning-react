eafd371e1c848fa9e269a7204bc2b0eaeee88019
Mir Haris Tauqeer <mirharis.tauqeer@gmail.com>
2025-03-18 13:52:50 +0500

    added component and usedprops with tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [counter, setCounter]  = useState(15)
+  {/* It is the Counter using useState() */ }
+  // const [counter, setCounter]  = useState(15)
 
-  const addValue = () => {
-    //counter = counter + 1
-    setCounter(prevCounter => prevCounter + 1)
-    
-  }
+  // const addValue = () => {
+  //   setCounter(prevCounter => prevCounter + 1)
 
-  const removeValue = () => {
-    setCounter(counter - 1)
+  // }
+
+  // const removeValue = () => {
+  //   setCounter(counter - 1)
+  // }
+  let myObj = {
+    name: 'Haris',
+    age: 18
   }
 
   return (
     <>
-      <h1>haris and react</h1>
+      {/* It is the Counter using useState() */}
+      {/* <h1>haris and react</h1>
       <h2>Counter value: {counter}</h2>
 
       <button
@@ -28,9 +32,15 @@ function App() {
       <button
       onClick={removeValue}
       >remove value {counter}</button>
-      <p>footer: {counter}</p>
+      <p>footer: {counter}</p> */}
+
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="Haris" btnText="Let's Go" />
+      <Card username="Rizwan" />
     </>
   )
 }
+
+
 
 export default App
